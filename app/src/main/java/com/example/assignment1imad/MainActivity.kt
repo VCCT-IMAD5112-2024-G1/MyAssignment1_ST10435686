7731c023dc6b86488d8e94c4000d6f6092b47824
marcelsnyman <ST10435686@vcconnect.edu.za>
2024-04-05 21:58:16 +0200

    Commit Message Initial Variable Declaration

--- app/src/main/java/com/example/assignment1imad/MainActivity.kt
+++ app/src/main/java/com/example/assignment1imad/MainActivity.kt
@@ -11,88 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // VARIABLE DECLARATION
-
         val textAgeInput = findViewById<EditText>(R.id.edtAgeInput)
         val generateButton = findViewById<Button>(R.id.btnGenerate)
         val clearButton = findViewById<Button>(R.id.btnClear)
         val textOutput = findViewById<TextView>(R.id.textOutput)
-
-        // GENERATE BUTTON
-        // THE BUTTON THAT GENERATES THE INFORMATION WITH THE INPUT AND RECIPROCATES IT INTO THE OUTPUT
-
-        generateButton?.setOnClickListener {
-
-            //VARIABLE DECLARATION
-
-            var strPerson: String = ""
-            var strHistoryAge: String = ""
-            var intAge : Int
-
-            intAge = textAgeInput.text.toString().toInt()
-
-            // IF STATEMENT TO CHECK WHETHER THE INTEGER VALUE GIVEN IS WITHIN THE REQUIRED RANGE 1
-
-            if (intAge >= 20 && intAge <= 100) {
-
-                //PROCESSING OF FINDING A MATCHING INTEGER
-
-                if (intAge == 25) {
-                    strPerson = "Tupac Shakur"
-                    strHistoryAge = "25"
-                } else if (intAge == 35) {
-                    strPerson = "Wolfgang Amadeus Mozart"
-                    strHistoryAge = "35"
-                } else if (intAge == 42) {
-                    strPerson = "Elvis Presley"
-                    strHistoryAge = "42"
-                } else if (intAge == 45) {
-                    strPerson = "Freddy Mercury"
-                    strHistoryAge = "45"
-                } else if (intAge == 51) {
-                    strPerson = "Napoléon Bonaparte"
-                    strHistoryAge = "51"
-                } else if (intAge == 56) {
-                    strPerson = "Abraham Lincoln"
-                    strHistoryAge = "56"
-                } else if (intAge == 67) {
-                    strPerson = "George Washington"
-                    strHistoryAge = "67"
-                } else if (intAge == 73) {
-                    strPerson = "Charles Darwin"
-                    strHistoryAge = "73"
-                } else if (intAge == 83) {
-                    strPerson = "Thomas Jefferson"
-                    strHistoryAge = "83"
-                } else if (intAge == 95) {
-                    strPerson = "Nelson Mandela"
-                    strHistoryAge = "95"
-                }
-
-                //THE OUTPUT OF THE FINAL INFORMATION THAT HAS BEEN SELECTED IF THE AGE THAT IS PROVIDED MATCHES WITH ONE OF THE HISTORICAL FIGURES AGES
-
-                textOutput.setText(strPerson + " died at the age of " + strHistoryAge + ". This is the same age which you are currently! They are a famous historical figure.")
-
-                //AN IF STATEMENT THAT DISPLAYS A MESSAGE IF THE AGE DOES NOT MATCH ANY OF THE HISTORICAL FIGURES AGES
-
-                if (intAge != 25 && intAge != 35 && intAge != 42 && intAge != 45 && intAge != 51 && intAge != 56 && intAge != 67 && intAge != 73 && intAge != 83 && intAge != 95) {
-                    textOutput.setText("There are no famous historical figures who share the same age as you do. Please try a different age!")
-                }
-            } else{
-
-                // ERROR MESSAGE OUTPUT FOR THE RANGE CHECK
-
-                textOutput.setText("Invalid age! Please use an age between 20 and 100!")
-            }
-        }
-
-        // CLEAR BUTTON
-        //CLEARS ALL THE INFORMATION IN THE INPUT AND OUTPUT SECTIONS
-
-        clearButton?.setOnClickListener{
-            textAgeInput.setText("")
-            textOutput.setText("")
-        }
 
     }
 
